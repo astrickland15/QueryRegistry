fee7b510aa57e2e637bdcc1438e34c47047a0a60
Anthony Strickland <astrickland15@hotmail.com>
2021-03-12 13:04:08 -0500

    Initial Upload

---
+++ QueryRegistry/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace QueryRegistry
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.UpdateComputers = new System.Windows.Forms.Button();
+            this.CheckMyPC = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // UpdateComputers
+            // 
+            this.UpdateComputers.Location = new System.Drawing.Point(140, 111);
+            this.UpdateComputers.Name = "UpdateComputers";
+            this.UpdateComputers.Size = new System.Drawing.Size(136, 23);
+            this.UpdateComputers.TabIndex = 0;
+            this.UpdateComputers.Text = "Update Computers";
+            this.UpdateComputers.UseVisualStyleBackColor = true;
+            this.UpdateComputers.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // CheckMyPC
+            // 
+            this.CheckMyPC.Location = new System.Drawing.Point(140, 50);
+            this.CheckMyPC.Name = "CheckMyPC";
+            this.CheckMyPC.Size = new System.Drawing.Size(136, 23);
+            this.CheckMyPC.TabIndex = 1;
+            this.CheckMyPC.Text = "Check PCs";
+            this.CheckMyPC.UseVisualStyleBackColor = true;
+            this.CheckMyPC.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.CheckMyPC);
+            this.Controls.Add(this.UpdateComputers);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button UpdateComputers;
+        private System.Windows.Forms.Button CheckMyPC;
+    }
+}
+
---
+++ QueryRegistry/Form1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Win32;
+using System.ServiceProcess;
+
+
+namespace QueryRegistry
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        List<string> UpdatedComputers = new List<string>();
+        List<string> ComputersRequringKeys = new List<string>();
+
+        List<string> ComputerList = new List<string>()
+            {
+            "ACCOUNTING-02",
+            "BDM-01",
+            "blake-02",
+            "CSM-04",
+            "CSM-05",
+            "DEV-03",
+            "DEV-04",
+            "DEV-06",
+            "Dev-07",
+            "studio-01",
+            "mike-02",
+            "MKTG-01",
+            "MKTG-02",
+            "NSCEO-04",
+            "OFFICEADMIN-01",
+            "rick-02",
+            "CSM-07",
+            "BUILDING-01",
+            "MKTG-07",
+            "MKTG-08",
+            "DATAANALYST-01"
+
+
+        };
+        //Update PCs
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            foreach (string computer in ComputerList)
+            {
+                try
+                {
+                    // set registry path and set it as writable
+                    RegistryKey path = RegistryKey.OpenRemoteBaseKey(RegistryHive.LocalMachine, computer).OpenSubKey
+                        ("SOFTWARE\\Policies\\Microsoft\\Windows", true);
+
+                    if (path == null)
+                    {
+                        //create WindowsUpdate subkey
+                        RegistryKey WUkey = path.CreateSubKey("WindowsUpdate");
+
+                        // Open WindowsUpdate Key
+                        WUkey.OpenSubKey("WindowsUpdate");
+
+                        //write to remote WindowsUpdate key
+                        WUkey.SetValue("WUServer", "http://192.168.0.50:8530", RegistryValueKind.String);
+                        WUkey.SetValue("WUStatusServer", "http://192.168.0.50:8530", RegistryValueKind.String);
+                        WUkey.SetValue("ElevateNonAdmins", 1, RegistryValueKind.DWord);
+                        WUkey.SetValue("TargetGroupEnabled", 1, RegistryValueKind.DWord);
+
+                        if (computer.ToLower() == "dataanalyst-01")
+                        {
+                            WUkey.SetValue("TargetGroup", "WU Test Machine", RegistryValueKind.String);
+                        }
+
+                        else
+                        {
+                            WUkey.SetValue("TargetGroup", "WSUS_Win10Computers", RegistryValueKind.String);
+                        }
+
+                        // add AU subkey
+                        RegistryKey AUkey = WUkey.CreateSubKey("AU");
+
+                        // write to remote AU key
+                        AUkey.SetValue("IncludeRecommendedUpdates", 1, RegistryValueKind.DWord);
+                        AUkey.SetValue("UseWUServer", 1, RegistryValueKind.DWord);
+                        AUkey.SetValue("DetectionFrequencyEnabled", 1, RegistryValueKind.DWord);
+                        AUkey.SetValue("DetectionFrequency", 4, RegistryValueKind.DWord);
+                        AUkey.SetValue("AutoInstallMinorUpdates", 1, RegistryValueKind.DWord);
+                        AUkey.SetValue("RescheduleWaitTimeEnabled", 0, RegistryValueKind.DWord);
+                        AUkey.SetValue("NoAutoRebootWithLoggedOnUsers", 1, RegistryValueKind.DWord);
+                        AUkey.SetValue("NoAutoUpdate", 0, RegistryValueKind.DWord);
+                        AUkey.SetValue("AUOptions", 4, RegistryValueKind.DWord);
+                        AUkey.SetValue("ScheduledInstallDay", 4, RegistryValueKind.DWord);
+                        AUkey.SetValue("ScheduledInstallTime", 16, RegistryValueKind.DWord);
+
+                        UpdatedComputers.Add(computer);
+
+
+                    }
+                }
+
+                catch
+                {
+                    MessageBox.Show(String.Format("hmmm, {0} is not connected to the Network!", computer), "Rut Roh");
+                }
+            }
+            //confirmation of updated computers
+            var message = DisplayListOfComputers(UpdatedComputers);
+            if (message != "")
+            {
+                MessageBox.Show("Updated Registry for: \n\n" + message);
+            }
+            else
+            {
+                MessageBox.Show("No Updates Necessary.");
+            }
+
+        }
+
+
+        //check PCs
+        private void button2_Click(object sender, EventArgs e)
+        {
+            foreach (string computer in ComputerList)
+            {
+                try
+                {
+                    RegistryKey remotePath = RegistryKey.OpenRemoteBaseKey(RegistryHive.LocalMachine, computer).OpenSubKey
+                        ("SOFTWARE\\Policies\\Microsoft\\Windows\\WindowsUpdate", true);
+
+
+
+                    if (remotePath == null)
+                    {
+                        ComputersRequringKeys.Add(computer);
+                    }
+
+                }
+                catch
+                {
+                    MessageBox.Show("Hmmm, can't connect to " + computer);
+                }
+            }
+            //confirmation of updated computers
+            var message = DisplayListOfComputers(ComputersRequringKeys);
+            if (message != "")
+            {
+                MessageBox.Show("Keys Missing for the Following: \n\n" + message);
+            }
+            else
+            {
+                MessageBox.Show("All Computers Up to Date");
+            }
+
+            MessageBox.Show("Check Complete");
+
+
+        }
+
+
+        public static String DisplayListOfComputers(List<string> testList)
+        {
+            return String.Join(Environment.NewLine, testList);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
